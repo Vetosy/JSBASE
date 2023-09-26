6d547771d42c79e6b51eebd816b9ff6b99116f39
Vetosy <xigga11@gmail.com>
2023-09-26 17:37:07 +0300

    added task

--- src/js/components/loops.js
+++ src/js/components/loops.js
@@ -1,2 +1,16 @@
 /////// Циклы
 
+console.log('stat');
+
+
+const myPromise = new Promise((resolve, reject) {
+  console.log(1);
+  resolve
+  console.log(3);
+})
+
+const res = () => {
+
+}
+
+console.log('end');
